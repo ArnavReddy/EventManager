7a36e30edddc5511ed0a97a2b2f7dc3714acd29d
Arnav Reddy <arnavreddy@gmail.com>
2019-08-17 15:59:06 -0700

    login/signup and write to mongo

---
+++ server.js
@@ -0,0 +1,96 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var fs = require("fs");
+var mongo = require("mongodb");
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+var signInemail;
+var signInpass;
+var signUpemail;
+var signUppass;
+var aae;
+
+app.get("/", function(req, res) {
+    res.redirect("login.html");
+});
+
+app.use(express.static("public"));
+
+app.get("/login.html", function(req, res) {
+    res.sendFile(__dirname + "/" + "login.html");
+});
+
+app.get("/process_signin", function(req, res) {
+    signInemail = req.query.first_name;
+    signInpass = req.query.last_name;
+    console.log("sign in: " + signInemail + ", " + signInpass);
+});
+
+app.get("/process_signup", function(req, res) {
+    signUpemail = req.query.first_name;
+    signUppass = req.query.last_name;
+
+    console.log("sign up: " + signUpemail + ", " + signUppass);
+    displayUsers();
+    check();
+});
+
+function addAccount() {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myobj = {
+            username: signUpemail,
+            password: signUppass
+        };
+        dbo.collection("Users").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+    displayUsers();
+}
+
+function displayUsers() {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("Users")
+            .find({})
+            .toArray(function(err, result) {
+                if (err) throw err;
+                db.close();
+            });
+    });
+}
+
+function check() {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var query = {
+            username: signUpemail
+        };
+        dbo.collection("Users")
+            .find(query)
+            .toArray(function(err, result) {
+                if (err) throw err;
+                if (!(typeof result === "undefined")) {
+                    aae = true;
+                    if (!aae) {
+                        addAccount();
+                    }
+                }
+                db.close();
+            });
+    });
+}
+
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
